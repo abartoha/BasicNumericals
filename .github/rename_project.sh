c6d3653b53086e1c3b18c85c5b9cd945dfdedecb
abartoha <abartoha@users.noreply.github.com>
2022-10-02 13:14:44 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="abartoha"
+original_name="basicnumericals"
+original_urlname="BasicNumericals"
+original_description="Awesome basicnumericals created by abartoha"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv basicnumericals $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- basicnumericals/__main__.py
+++ basicnumericals/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for basicnumericals."""
 
 from .cli import main  # pragma: no cover
 
